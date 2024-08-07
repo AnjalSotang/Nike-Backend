12c43adb63b85b8ea9d4ddfb965a7ca631d5175a
AnjalSotang <anjalsotang26@gmail.com>
2024-08-07 15:11:52 +0545

    Created a middleware called upload.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,38 +1,8 @@
 const express = require("express")
-const multer = require("multer")
 const {findProfileById, updateProfile} = require("../controllers/profile");
 const { checkTokenAndRole } = require("../middleware/checkTokenAndRole");
+const upload = require("../middleware/upload"); // Import the upload middleware
 const router = express.Router()
-
-//Defining the storage configuration for upload files using multer.diskStorage()
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "./uploads");
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + file.originalname);
-    },
-  });
-  
-  const fileFilter = (req, file, cb) => {
-    if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/png"
-    ) {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
-  
-  var upload = multer({
-    storage: storage,
-    limits: {
-      fileSize: 1024 * 1024 * 5,
-    },
-    fileFilter: fileFilter,
-  });
 
 
 router.post('/updateProfile', upload.single("image"), checkTokenAndRole("user"), updateProfile);
