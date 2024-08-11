e1ae3d101fa0ddfb9efec37ef21171ebef8c0ca7
AnjalSotang <anjalsotang26@gmail.com>
2024-08-11 08:16:31 +0545

    Wish is ready

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,12 +1,12 @@
 const express = require("express")
-const {createWish, findWishById, findAllWish, updateWish, deleteWish} = require("../controllers/wishlist");
+const {createWish, findWishById, deleteWish} = require("../controllers/wishlist");
 const { checkTokenAndRole } = require("../middleware/checkTokenAndRole");
 const router = express.Router()
 
 
 router.post('/createWish', checkTokenAndRole("user"), createWish);
 router.get('/viewWish',checkTokenAndRole("user"), findWishById);
-router.delete('/deleteWish', checkTokenAndRole("user", deleteWish))
+router.delete('/deleteWish', checkTokenAndRole("user", deleteWish));
 
 module.exports= router;
 
