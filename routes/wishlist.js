c96030bceb46b658cc46ca3c17c62cc7340e2c28
AnjalSotang <anjalsotang26@gmail.com>
2024-08-14 09:25:19 +0545

    push

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -6,6 +6,6 @@ const router = express.Router()
 
 router.post('/createWish/:id', checkTokenAndRole("user"), createWish);
 router.get('/viewWish',checkTokenAndRole("user"), findWishById);
-router.delete('/deleteWish/:id', checkTokenAndRole("user", deleteWish));
+router.delete('/deleteWish/:id', checkTokenAndRole("user"), deleteWish);
 
 module.exports= router;
