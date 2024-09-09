872d2ee106811e96f3577a859e5bc30aeb066504
AnjalSotang <anjalsotang26@gmail.com>
2024-09-09 09:15:35 +0545

    Payment Integration

--- routes/payment.js
+++ routes/payment.js
@@ -3,6 +3,6 @@ const { payment } = require("../controllers/payment")
 const { checkTokenAndRole } = require("../middleware/checkTokenAndRole");
 const router = express.Router()
 
-router.post('/charge', checkTokenAndRole("user"), payment);
+router.post('/charge/:id', checkTokenAndRole("user"), payment);
 
 module.exports = router;
